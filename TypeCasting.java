1369202de4c2adff492149ad0eb8f06270f56b23
SyedArizHaider <132950532+SyedArizHaider@users.noreply.github.com>
2023-05-08 23:39:10 -0500

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,30 @@
+package assistedpractice;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		        char a='A';
+		        int i=a;
+		        float f=a;
+		        long l=a;
+		        double d=a;
+		        System.out.println("implicit type casting");
+		        System.out.println("before implicit conversion :" + a);
+		        System.out.println("after implicit conversion  : " + i);
+		        System.out.println("after implicit conversion  : " + f);
+		        System.out.println("after implicit conversion  : " + l);
+		        System.out.println("after implicit conversion  : " + d);
+		        
+
+		         double h=5;
+		         int r=(int)h;
+
+		System.out.println("explicit typecasting");
+		        System.out.println("before explicit conversion :" +h );
+		        System.out.println("after explicit conversion ,  : " + r);
+
+
+
+		    }
+		
+	}
