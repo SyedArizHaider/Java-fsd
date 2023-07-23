c6529a074a62e06533b7b9c0512b65dc3e3bdebd
SyedArizHadider <ariznaqvi22@gmail.com>
2023-07-24 00:05:46 +0530

    new

---
+++ phase 5/Selenium_practice_project/src/main/java/HelloWorldSelenium.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.*;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public class HelloWorldSelenium {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		String baseUrl="https://www.google.com";
+		driver.get(baseUrl);
+		
+	}
+
+}
